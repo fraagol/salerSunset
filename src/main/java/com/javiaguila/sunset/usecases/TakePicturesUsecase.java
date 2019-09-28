8e4f49b987a6fc67e60738395e519cfdea376521
fraagol <qwqwasasQ!1>
2019-09-29 00:38:24 +0200

    Gif generator

--- src/main/java/com/javiaguila/sunset/usecases/TakePicturesUsecase.java
+++ src/main/java/com/javiaguila/sunset/usecases/TakePicturesUsecase.java
@@ -3,6 +3,8 @@ package com.javiaguila.sunset.usecases;
 import com.javiaguila.sunset.domain.Picture;
 import com.javiaguila.sunset.usecases.adapters.CameraAdapter;
 
+import java.io.File;
+
 public class TakePicturesUsecase {
 
     private CameraAdapter cameraAdapter;
@@ -14,8 +16,17 @@ public class TakePicturesUsecase {
     public void takePictures() throws Exception {
 
         for (int i = 0; i < 10; i++) {
-            System.out.println("Calling adapter for picture "+i);
-            Picture picture = cameraAdapter.takePicture("picture"+i+".jpg");
+            System.out.println("Calling adapter for picture " + i);
+            Picture picture = cameraAdapter.takePicture("picture" + i + ".jpg");
         }
+
+    }
+
+    public void generateGif() {
+
+    }
+
+    private File[] getPicturesFilesForGif() {
+        return null;
     }
 }
